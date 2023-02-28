d5198cb127bc82e51715a5b2b7bfa55beb90a0c8
Michael Plumlee <michaelplumlee95@gmail.com>
2023-02-28 15:44:50 -0800

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+function writeCards(listOfNames, event) {
+    const messages = []
+    for (let i = 0; i < listOfNames.length; i++) {
+        messages.push(`Thank you, ${listOfNames[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages
+}
+
+function countDown(n) {
+    while (n >= 0)
+    console.log(n--)
+}
+
+countDown(10);
